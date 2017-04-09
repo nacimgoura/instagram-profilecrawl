c227697c60735ed295bcfb93182dc5bb0935bd07
Nacim Goura <nacim.goura@gmail.com>
2017-04-09 20:59:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,73 @@
+<img src="https://s3-eu-central-1.amazonaws.com/centaur-wp/designweek/prod/content/uploads/2016/05/11170038/Instagram_Logo-1002x1003.jpg" width="200" align="right">
+
+# Instagram-Profilecrawl
+
+## Quickly crawl the information (e.g. followers, tags etc...) of an instagram profile. No login required!
+Automation Script for crawling information from ones instagram profile.  
+Like e.g. the number of posts, followers, and the tags of the the posts
+
+[![Build Status](https://travis-ci.org/nacimgoura/instagram-profilecrawl.svg?branch=master)](https://travis-ci.org/nacimgoura/instagram-profilecrawl)
+[![built with NodeJS](https://img.shields.io/badge/Built%20with-nodejs-green.svg)](https://www.nodejs.org/)
+[![built with Selenium](https://img.shields.io/badge/built%20with-Selenium-red.svg)](https://github.com/SeleniumHQ/selenium)
+
+Note : this is a fork of 
+[instagram-profilecrawl](https://github.com/timgrossmann/instagram-profilecrawl)
+created by [timgrossmann](https://github.com/timgrossmann).
+I'm not a python developer but I was very interested in this project so I realized
+ the same thing but in nodeJS, bringing some improvements.
+
+#### Getting started
+Just do:
+```bash
+npm install --g instagram-profilecrawl
+
+Usage
+	$ instagram-profile-crawl <name>
+	  
+	Options
+	  --chrome      use chrome
+	  --firefox     use firefox
+
+	Examples
+	  $ instagram-profile-crawl nacim_goura
+```
+The information will be saved in a JSON-File same profile {username}.json.
+
+Important : Even if it is possible to use different browser, I advise
+to use chrome because I realized this application on chrome and I did
+not take the time to test on others.
+
+Structure of data : 
+```
+{
+  "alias": "nacim_goura",
+  "username": "Nacim",
+  "description": "French student",
+  "urlImgProfile": "https://scontent-cdg2-1.cdninstagram.com/t51.2885-19/s150x150/17662950_1416155488436522_4443366366061264896_a.jpg",
+  "numberOfPosts": "21",
+  "numberFollowers": "391",
+  "numberFollowing": "407",
+  "private": false,
+  "posts": [
+        {
+          "url": "https://www.instagram.com/p/BShIo0IAg00/?taken-by=nacim_goura",
+          "urlImage": "https://scontent-cdg2-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/c135.0.810.810/17818002_126533407886446_8602338629349539840_n.jpg",
+          "numberLikes": "44",
+          "numberComments": "1",
+          "isVideo": false,
+          "description": "Souvenir d'été 🌞",
+          "tags": [
+            "#landscape",
+            "#nofilter",
+            "#sun",
+            "#summer",
+            "#heat",
+            "#nostalgia",
+            "#morocco",
+            "#home"
+          ]
+        },
+        ...
+  ]      
+}
+```
---
+++ index.js
@@ -0,0 +1,170 @@
+#!/usr/bin/env node
+
+const selenium = require('selenium-standalone');
+const webdriver = require('webdriverio');
+const fs = require('fs');
+const meow = require('meow');
+const ora = require('ora');
+const chalk = require('chalk');
+
+const cli = meow(`
+	Usage
+	  $ instagram-profile-crawl <name>
+	  
+	Options
+	  --chrome      use chrome
+	  --firefox     use firefox
+
+	Examples
+	  $ instagram-profile-crawl nacim_goura
+`, {
+	default: {chrome: true},
+});
+
+const spinner = ora('Loading Profile!').start();
+if(cli.flags && !['chrome', 'firefox'].includes(Object.keys(cli.flags)[0])) {
+	return spinner.fail(chalk.red('Invalid browser!'));
+}
+const name = cli.input[0];
+if (!name) {
+	return spinner.fail(chalk.red('No name entered!'));
+}
+
+const options = {
+	desiredCapabilities: {
+		browserName: Object.keys(cli.flags)[0],
+	},
+};
+
+const browser = webdriver
+	.remote(options)
+	.init()
+	.url(`http://www.instagram.com/${name}`)
+	.then(() => {
+		browser.deleteCookie();
+		extractDataProfile.run(browser);
+	});
+
+const extractDataProfile = {
+
+	async run(browser) {
+		spinner.text = 'Start of treatment!';
+
+		this.data = {
+			alias: await this.getValue('h1'),
+			username: await this.getValue('h2._79dar'),
+			description: await this.getValue('._bugdy span'),
+			urlImgProfile: await this.getValue('._o0ohn img', 'attribute', 'src'),
+			numberOfPosts: await this.getValue('ul._9o0bc li:first-child ._bkw5z', 'text', 0),
+			numberFollowers: await this.getValue('ul._9o0bc li:nth-child(2) ._bkw5z', 'text', '', 0),
+			numberFollowing: await this.getValue('ul._9o0bc li:nth-child(3) ._bkw5z', 'text', '', 0),
+			private: await browser.isVisible('h2._glq0k'),
+			posts: [],
+		};
+
+		if (await browser.isExisting('div.error-container')) {
+			return this.failInProfile('This profile does not exist!');
+		}
+
+		if(this.data.private === true) {
+			this.createFile();
+		}else {
+			await this.extractUrlPostProfile();
+		}
+	},
+
+	// get value in page and catch error
+	async getValue(element, type = 'text', typeAttribute = '', defaultValue = '') {
+		const isExisting = await browser.isExisting(element);
+
+		if (isExisting) {
+			switch (type) {
+				case 'text':
+					return browser.getText(element);
+				case 'attribute':
+					return browser.getAttribute(element, typeAttribute);
+				default:
+					return browser.getText(element);
+			}
+		}
+		return defaultValue;
+	},
+
+	// extract all image url from profile
+	async extractUrlPostProfile() {
+		const self = this;
+
+		let urls = await this.getValue('._nljxa a', 'attribute', 'href', []);
+		if (urls.length > 0 && this.data.numberOfPosts > 12) {
+			await this.loadButton();
+			while (Number(this.data.numberOfPosts) !== urls.length) {
+				await browser.scroll('body', 0, 15000).pause(1000);
+				urls = await this.getValue('._nljxa a', 'attribute', 'href', []);
+			}
+		}
+		let i = 1;
+		let j = 1;
+
+		async function moveToObject() {
+			if (self.data.posts.length === self.data.numberOfPosts - 1) {
+				return self.createFile();
+			}
+
+			try {
+				const item = `._nljxa ._myci9:nth-child(${i}) a:nth-child(${j})`;
+				browser.moveToObject(item)
+					.then(async () => {
+						const post = {
+							url: await self.getValue(item, 'attribute', 'href'),
+							urlImage: await self.getValue(`${item} img`, 'attribute', 'src'),
+							numberLikes: await self.getValue(`${item} li._sjq6j span:first-child`, 'text', '', 0),
+							numberComments: await self.getValue(`${item} li._qq2if span:first-child`, 'text', '', 0),
+							isVideo: await browser.isVisible(`${item} ._qihym`),
+						};
+						const description = await self.getValue(`${item} img`, 'attribute', 'alt');
+						post.description = description.replace(/#[a-z\u00E0-\u00FC]*/g, '').trim();
+						post.tags = description.split(' ').filter(n => /^#/.exec(n));
+						self.data.posts.push(post);
+
+						if (j === 3) {
+							j = 1;
+							i++;
+						} else {
+							j++;
+						}
+
+						spinner.text = `${self.data.posts.length} posts processed!`;
+
+						await moveToObject();
+					});
+			}catch (error) {
+				this.failInProfile(error);
+			}
+
+		}
+
+		await moveToObject();
+	},
+
+	// click on load button
+	async loadButton() {
+		const isVisible = await browser.isExisting('a._8imhp');
+		if (isVisible) {
+			await browser.click('a._8imhp');
+		}
+	},
+
+	// create final file
+	async createFile() {
+		fs.writeFile(`profile ${this.data.alias}.json`, JSON.stringify(this.data, null, 2), 'utf-8', (err) => {
+			if (err) throw err;
+			spinner.succeed(chalk.green('File created with success!'));
+			browser.end();
+		});
+	},
+
+	failInProfile(message) {
+		browser.end();
+		return spinner.fail(chalk.red(`Error : ${message}`));
+	},
+};
